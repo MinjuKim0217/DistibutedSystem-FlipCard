054b0903ab0ad2b36910b44244c70ab0a80b0eb4
Minju Kim <76491201+MinjuKim0217@users.noreply.github.com>
2021-05-20 20:17:22 +0900

    Create FlipCardClientEventHandler.java
    이벤트 핸들러 수정

---
+++ FlipCardClientEventHandler.java
@@ -0,0 +1,199 @@
+import java.util.Iterator;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+import kr.ac.konkuk.ccslab.cm.entity.CMGroup;
+import kr.ac.konkuk.ccslab.cm.entity.CMGroupInfo;
+import kr.ac.konkuk.ccslab.cm.entity.CMMember;
+import kr.ac.konkuk.ccslab.cm.entity.CMSessionInfo;
+import kr.ac.konkuk.ccslab.cm.entity.CMUser;
+import kr.ac.konkuk.ccslab.cm.event.CMDataEvent;
+import kr.ac.konkuk.ccslab.cm.event.CMDummyEvent;
+import kr.ac.konkuk.ccslab.cm.event.CMEvent;
+import kr.ac.konkuk.ccslab.cm.event.CMSessionEvent;
+import kr.ac.konkuk.ccslab.cm.event.handler.CMAppEventHandler;
+import kr.ac.konkuk.ccslab.cm.info.CMInfo;
+import kr.ac.konkuk.ccslab.cm.info.CMInteractionInfo;
+import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
+
+public class FlipCardClientEventHandler implements CMAppEventHandler {
+	private CMClientStub m_clientStub;
+	private FlipCardClient m_client;
+	Maingame game = new Maingame();
+	
+
+	public void getClientEventHandler(CMClientStub stub, FlipCardClient client) {
+		m_client = client;
+		m_clientStub = stub;
+	}
+
+	public void processEvent(CMEvent cme) {
+		// TODO Auto-generated method stub
+		// System.out.println("processEvent: Client app receives CM event!!");
+		switch (cme.getType()) {
+		case CMInfo.CM_DUMMY_EVENT:
+			ReceiveprocessDummyEvent(cme);
+			break;
+		case CMInfo.CM_SESSION_EVENT:
+			processSessionEvent(cme);
+
+		default:
+			break;
+		}
+	}
+
+	private void ReceiveprocessDummyEvent(CMEvent cme) {
+		CMDummyEvent cde = (CMDummyEvent) cme; // 서버에게서 받는
+		String message = cde.getDummyInfo();
+		String[] splitMsg = message.split(";");
+		String choice = splitMsg[0];
+		User MyUser = new User();
+		CMUser CUser=new CMUser();
+		switch (choice) {
+		case "LOGIN":
+			String UserID = splitMsg[1];
+			String Color = splitMsg[2];
+			
+			//FlipCardClient newgame = new FlipCardClient();
+			MyUser.ChangeCurrentUser(UserID, Color);
+			// newgame.FlipCardClientGame();
+
+			// 메인게임 불러오기-연결만
+			if(UserID==CUser.getName()) {
+				MyUser.GetMyuserColor(UserID);
+				FlipCardClient changeflag = new FlipCardClient();
+				changeflag.openflag = true;
+				Maingame frame = new Maingame(); //maingame 창 띄움
+	             frame.setVisible(true);
+			}
+			
+			
+		case "START":
+			game.init();
+			game.startflag = true;
+			// 카드 초기화
+			// 시간 10초로 보여짐
+
+		case "FLIP":
+			int Color1 = 0;
+			switch (splitMsg[2]) {
+			case "BROWN":
+				Color1 = 1;
+				break;
+			case "BLUE":
+				Color1 = 2;
+				break;
+			case "PINK":
+				Color1 = 3;
+				break;
+			case "GREEN":
+				Color1 = 4;
+				break;
+			}
+			game.ChangeColor(Color1, Integer.parseInt(splitMsg[1]));
+			// 카드 변경
+		case "STOP":
+			game.startflag = false;
+			// 시간 0초로 보여짐
+			// 카드 초기화
+			// 클라측 이벤트 핸들러 중단
+		case "WIN":
+			// 채팅창에 표시
+
+		}
+
+	}
+
+	private void sendprocessDummyEvent(int CardNum) {
+		// Client 측 전송 Flip;CARDNUM;COLOR
+		CMDummyEvent due = new CMDummyEvent();// 서버에게 주는
+		User MyUser = new User();
+		if (game.startflag) {
+			due.setDummyInfo(MakeFlip(CardNum, MyUser.GetMyColor()));
+			m_clientStub.send(due, "SERVER");
+		}
+	}
+	
+	public void processSessionEvent(CMEvent cme) {
+		CMSessionEvent se=(CMSessionEvent) cme;
+		switch(se.getID()) {
+		case CMSessionEvent.LOGIN_ACK:
+			if(se.isValidUser()==0) {
+				System.err.println("client failed to login");
+				//Login.loginSendcheck=false;
+				m_clientStub.terminateCM();
+				
+			}
+			
+			else if(se.isValidUser()==-1) {
+				System.err.println("already exist!");
+			
+				
+			}
+			else {
+				System.out.println("login complete!");
+				//m_client.FlipCardClientGame(m_clientStub);
+				//Login.loginSendcheck=true;
+				//FlipCardClient id=new FlipCardClient();
+				
+				//User newuser=new User();
+				//newuser.GetMyuserColor(m_client.uname);
+				
+				
+				}
+			break;
+		default:
+			return;
+		}
+		
+	}
+	
+	
+	private void processDataEvent(CMEvent cme) {
+		CMDataEvent de = (CMDataEvent) cme;
+		switch (de.getID()) {
+		case CMDataEvent.NEW_USER:
+			printMessage("[" + de.getUserName() + "] 의 사용자가 들어왔습니다..", "Chat");
+			// printMessage(count+"", "Info");
+
+			break;
+		case CMDataEvent.REMOVE_USER:
+			// printMessage("["+de.getUserName()+"] leaves group("+de.getHandlerGroup()+")
+			// in session("
+			// +de.getHandlerSession()+").");
+			printMessage("[" + de.getUserName() + "] 의 사용자가 나갔습니다.", "Chat");
+			// printMessage(count+"", "Info");
+			break;
+		default:
+			return;
+		}
+	}
+
+	private void printMessage(String strText, String type) {
+		// m_client.printMessage(strText, type);
+
+		return;
+	}
+
+	private String MakeFlip(int CardNum, int indexofColor) {
+		String Color = null;
+		switch (indexofColor) {
+		case 1:
+			Color = "BROWN";
+			break;
+		case 2:
+			Color = "BLUE";
+			break;
+		case 3:
+			Color = "PINK";
+			break;
+		case 4:
+			Color = "GREEN";
+			break;
+		}
+		String result = "FLIP" + ";" + Integer.toString(CardNum) + ";" + Color;
+		return result;
+	}
+
+}
